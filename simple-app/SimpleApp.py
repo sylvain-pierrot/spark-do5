0bf724d931d6157238c7734ee1cc4c2c86468d31
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2025-03-26 11:20:33 +0100

    fix: ssl cert
    Signed-off-by: sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>

--- simple-app/SimpleApp.py
+++ simple-app/SimpleApp.py
@@ -9,19 +9,24 @@ s3_bucket = os.environ["S3_BUCKET"]
 s3_file_path = os.environ["S3_FILE_PATH"] 
 
 spark = SparkSession.builder \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
     .appName("SimpleApp") \
-    .config("spark.hadoop.fs.s3a.endpoint", s3_endpoint) \
-    .config("spark.hadoop.fs.s3a.access.key", s3_access_key) \
-    .config("spark.hadoop.fs.s3a.secret.key", s3_secret_key) \
-    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "true") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
+sc = spark.sparkContext
+
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", s3_endpoint)
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", s3_access_key)
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", s3_secret_key)
+sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "true")
+sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "true")
 
 s3_uri = f"s3a://{s3_bucket}/{s3_file_path}"
-df = spark.read.option("header", "true").json(s3_uri)
+df = spark.read.text(s3_uri)
 
 df.show()
+
 # numAs = logData.filter(logData.value.contains('a')).count()
 # numBs = logData.filter(logData.value.contains('b')).count()
 
